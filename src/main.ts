1ad27ad8fda652f733d2221ce8917a873c96b797
hyuk <wold2180@gmail.com>
2025-09-30 12:33:53 +0900

    update: 로그 양식 변경

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import fastify from 'fastify';
+import { PORT, HOST } from './common/constants';
+import { loggerOptions } from './common/loggerOptions';
+
+const app = fastify({
+    logger: loggerOptions,
+});
+
+app.get('/', async (request, reply) => {
+    return { hello: 'world' };
+});
+
+const start = async () => {
+    try {
+        await app.listen({ port: PORT, host: HOST });
+        app.log.info(`****************************************************************`);
+        app.log.info(`LitUp API Server Started and Listening at http://${HOST}:${PORT}`);
+        app.log.info(`****************************************************************`);
+    } catch (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
+};
+start();
