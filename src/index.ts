b0505139a60c2278a4ace83e49eef3d28afcbc32
hyuk <wold2180@gmail.com>
2025-09-30 12:07:13 +0900

    update: hello world 띄우기 완료

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('Hello, Fastify with TypeScript!');
+import fastify from 'fastify'
+import pino from 'pino';
+
+const app = fastify(
+    {logger: {
+        level: 'info',
+        transport: {
+            target: 'pino-pretty',
+            options: {
+                colorize: true
+            }
+        }
+    }}
+);
+
+app.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+const start = async () => {
+  try {
+    await app.listen({ port: 3000 }) // { port: 3000, host: '0.0.0.0' }
+  } catch (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+}
+start()
