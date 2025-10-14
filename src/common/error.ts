d2817b700140051920ff456b29d207b56705aa69
hyuk <wold2180@gmail.com>
2025-10-14 15:07:10 +0900

    update: 공통 에러 핸들러 작성

---
+++ src/common/error.ts
@@ -0,0 +1,8 @@
+import createError from '@fastify/error';
+
+export const NotFoundError = createError('NOT_FOUND', '%s', 404);
+export const ConflictError = createError('CONFLICT', '%s', 409);
+export const BadRequestError = createError('BAD_REQUEST', '%s', 400);
+export const ForbiddenError = createError('FORBIDDEN', '%s', 403);
+export const InternalServerError = createError('INTERNAL_SERVER_ERROR', '%s', 500);
+export const UnauthorizedError = createError('UNAUTHORIZED', '%s', 401);
