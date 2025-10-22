26bc57115714a80785cf318dce76cbbd67fb2298
hyuk <wold2180@gmail.com>
2025-10-22 12:04:09 +0900

    update: env파일 읽는 부분 수정

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,15 +1,17 @@
 import path from 'path';
 import dotenv from 'dotenv';
 
-if (process.env.NODE_ENV === 'development') {
-    dotenv.config({ path: path.resolve('.env.development') });
-} else if (process.env.NODE_ENV === 'production') {
-    dotenv.config({ path: path.resolve('.env') });
-} else {
-    throw new Error(`NODE_ENV 미지정 오류`);
-}
-
 export const NODE_ENV = process.env.NODE_ENV || 'development';
+
+const envFile =
+    NODE_ENV === 'production'
+        ? '.env'
+        : NODE_ENV === 'development'
+          ? '.env.local'
+          : `.env.${NODE_ENV}`;
+
+dotenv.config({ path: path.resolve(process.cwd(), envFile) });
+
 export const PORT = Number(process.env.PORT) || 11000;
 export const HOST = process.env.HOST || '0.0.0.0';
 export const DATABASE_URL = process.env.DATABASE_URL || '';
