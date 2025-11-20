af5b84f2bb1e831c8b328830ab66f532f54a8d25
hyuk <wold2180@gmail.com>
2025-11-20 13:36:07 +0900

    update: 닉네임은 띄어쓰기 제외 최대 12자로 변경

--- src/main.ts
+++ src/main.ts
@@ -33,14 +33,18 @@ app.addHook('onClose', async (instance) => {
 
 const start = async () => {
     try {
-        const commonService = new NicknameService(app.prisma);
-        await commonService.init();
-
         await app.listen({ port: PORT, host: HOST });
         app.log.info(`****************************************************************`);
         app.log.info(`LitUp API Server Started and Listening at http://${HOST}:${PORT}/docs`);
         app.log.info(`Database URL: ${DATABASE_URL?.split('@')[1] || 'Not configured'}`); // 비밀번호 제외
         app.log.info(`****************************************************************`);
+
+        // 닉네임 생성에 사용할 단어 리스트 초기화
+        // await 처리를 하지 않아. 사용하는 곳에서 null 처리 필수
+        const commonService = new NicknameService(app.prisma);
+        commonService.init().catch((err) => {
+            app.log.error('닉네임 생성 클래스 초기화 실패', err);
+        });
     } catch (err) {
         app.log.error(err);
         process.exit(1);
