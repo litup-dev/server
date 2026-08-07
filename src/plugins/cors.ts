cb2d3dd16cad3c0fb3c639f29371ed7eb8b6d997
hyeok kim <53270168+wold21@users.noreply.github.com>
2026-08-07 09:30:34 +0900

    update: origins 설정

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -5,14 +5,24 @@ const allowedOrigins = [
     'http://100.116.32.24:11000',
     'http://localhost:11000',
     'http://127.0.0.1:11000',
+    'http://100.94.94.123',
+    'http://100.91.160.64',
+    'http://100.125.51.55', 
+    'http://100.109.228.88', 
     'https://litup.kr',
     'https://www.litup.kr',
 ];
 
 export async function registerCors(fastify: FastifyInstance) {
     await fastify.register(fastifyCors, {
+        //origin: (origin, cb) => {
+        //    return cb(null, true);
+        //},
         origin: (origin, cb) => {
-            return cb(null, true);
+            if (allowedOrigins.includes(origin)) {
+                return cb(null, true);
+            }
+            return cb(new Error('Not allowed by CORS'), false);
         },
         methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'x-internal-secret'],
